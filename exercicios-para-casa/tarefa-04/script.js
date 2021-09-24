44cd061c6ae149c7cc23355f53e8453e1c036155
fabiula-rodrigues <mfabi463@gmail.com>
2021-09-24 18:05:32 -0300

    feitoAlgumasTarefas

--- exercicios-para-casa/tarefa-04/script.js
+++ exercicios-para-casa/tarefa-04/script.js
@@ -4,3 +4,14 @@
     1. Receba o preço de cada um dos 10 itens;
     2. Mostre na tela a soma de todos os preços.
  */
+    function somaValores(){let p1= document.getElementeById("Digite o valor do produto1").value;
+    let p2= document.getElementeById("Digite o valor do produto2").value;
+    let p3= document.getElementeById("Digite o valor do produto3").value;
+    let p4= document.getElementeById("Digite o valor do produto4").value;
+    let p5= document.getElementeById("Digite o valor do produto5").value;
+    let p6= document.getElementeById("Digite o valor do produto6").value;
+    let p7= document.getElementeById("Digite o valor do produto7").value;
+    let p8= document.getElementeById("Digite o valor do produto8").value;
+    let p9= document.getElementeById("Digite o valor do produto9").value;
+    let p10= document.getElementeById("Digite o valor do produto10").value;
+    let total = p1+p2+p2+p3+p4+p5+p6+p7+p8+p9+p10; alert(total);
