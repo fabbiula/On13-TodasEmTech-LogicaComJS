77bb3466aa6bbac8e9dca9a4f94449a6fe3fa90e
fabiula-rodrigues <mfabi463@gmail.com>
2021-09-24 19:22:59 -0300

    adicionadotodoida

--- exercicios-para-casa/tarefa-06/script.js
+++ exercicios-para-casa/tarefa-06/script.js
@@ -5,3 +5,8 @@
     2. Informe o total da conta do cliente;
     3. Exiba uma mensagem com o valor do troco.
  */
+    let ano = prompt("Digite o ano do seu nascimento");
+    if(2021-ano >=18){ alert("Pode comprar bebida")
+    } else{
+      alert("Proibido vender bebida alcóolica para menores de idade")
+    }
